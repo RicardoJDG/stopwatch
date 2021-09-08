6aed0bf9092714dd3e07bf6dbeb2b001392ab1c2
Ricardo Delgado <ricardojdg13@gmailcom>
2021-09-08 16:48:10 +0200

    Tracking time with Date object and added Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Stopwatch project in vanilla js
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const lapBtn = document.getElementById('lapBtn');
+const stopBtn = document.getElementById('stopBtn');
 const startBtn = document.getElementById('startBtn');
 const timer = document.getElementById('timer');
 
-const startTime = (e) => {
-    e.preventDefault();
-    timer.innerHTML = 'Hello';
+var handle = null;
+var hiddenTimer = 0;
+
+const increment = (startTime) => {
+    const current = Date.now();
+    const time = current - startTime;
+    timer.textContent = Math.floor(time / 1000);
 }
 
-const stopTime = (e) => {
+function startTime (e) {
     e.preventDefault();
+    const start = Date.now();
+    handle = setInterval(() => increment(start), 10);
+    startBtn.classList.add('hidden');
+    stopBtn.classList.remove('hidden');
+}
+
+function stopTime (e) {
+    e.preventDefault();
+    clearInterval(handle);
+    startBtn.classList.remove('hidden');
+    stopBtn.classList.add('hidden');
+}
+
+function lap (e) {
+    e.preventDefault();
+    const ul = document.getElementById('timersList');
+    const li = document.createElement("li");
+    li.innerText = timer.textContent;
+    const lapTimers = [];
+    lapTimers.push(timer.textContent);
+    ul.appendChild(li)
+
 }
 
 startBtn.addEventListener('click', startTime);
+stopBtn.addEventListener('click', stopTime);
+lapBtn.addEventListener('click', lap);
